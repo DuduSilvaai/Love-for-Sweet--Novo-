42176c8b81f1bf97cd1e41886570810892f06eb6
DuduSilvaai <silvaduds395@gmail.com>
2025-10-12 21:11:12 -0300

    Update Footer.tsx
    Footer changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,13 +24,17 @@ const Footer = () => {
               </p>
               <div className="flex space-x-4">
                 <a
-                  href="#"
+                  href="https://www.instagram.com/loveforsweetoficial/"
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="w-10 h-10 border border-input bg-white/20 rounded-full flex items-center justify-center hover:bg-white/30 transition-colors"
                 >
                   <Instagram className="w-5 h-5" />
                 </a>
                 <a
-                  href="#"
+                  href="https://www.linkedin.com/in/love-for-sweet-317371387/"
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="w-10 h-10 bg-white/20 border border-input rounded-full flex items-center justify-center hover:bg-white/30 transition-colors"
                 >
                   <Linkedin className="w-5 h-5" />
